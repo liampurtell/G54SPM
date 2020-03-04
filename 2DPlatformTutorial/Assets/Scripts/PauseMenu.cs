538e96ec0065efdda63fcde38d179fd0cd6b708b
Shuo Wang <375089592@qq.com>
2020-03-04 17:19:01 +0000

    Pause panel

---
+++ 2DPlatformTutorial/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool GameIsPaused = false;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused)
+			{
+				Resume();
+			} else
+			{
+				Pause();
+			}
+        }
+
+	}
+}
